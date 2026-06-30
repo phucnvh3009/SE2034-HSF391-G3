d11eb6f6f108ccc598eda1ae29598eff90637d1c
Nguyen Vu Hoang Phuc <cohphuc1234509876@gmail.com>
2026-06-30 11:20:37 +0700

    tạo packet

---
+++ src/main/java/vn/edu/fpt/StudentDormitoryApplication.java
@@ -0,0 +1,16 @@
+package vn.edu.fpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDormitoryApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudentDormitoryApplication.class, args);
+        System.out.println("Student Dormitory Application Started");
+        System.out.println("Team 03");
+    }
+
+}
